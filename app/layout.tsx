ffe929d1edaaaf5ea97efcaf45bd0da8ffc4e8ae
RhymeTheRapper <rhymeyd@gmail.com>
2026-02-24 12:24:06 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Questrial, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const questrial = Questrial({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${questrial.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
